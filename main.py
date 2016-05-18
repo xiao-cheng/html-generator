10b7e0386bddd9630f05fe55eaf7c7b7690cbfa8
Xiao Cheng <xiao@cs.stanford.edu>
2016-05-17 21:36:04 -0700

    added paralle generation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Created on May 5, 2016
 '''
 from argparse import ArgumentParser
 from generate import *
+from multiprocessing import Pool
 
 
 if __name__ == '__main__':
@@ -15,12 +16,15 @@ if __name__ == '__main__':
                         help = "output directory, defaults to None and print to stdout")
     parser.add_argument("-batch", type = int, default = None,
                     help = "Generates all htmls with seed from 0 to this number")
+    parser.add_argument("-parallel", "-p", default = 4,
+                        help = "cores to use for parallel generation")
     args = parser.parse_args()
     seeds = [args.seed] if args.batch is None else xrange(args.batch)
     if args.batch is not None: args.output = None
-    for seed in seeds:
+    def run(seed):
         generated_html = create(seed)
         out_file = args.output if args.output else str(seed) + '.html'
     #         print generated_html.prettify()
         with open(out_file, 'w') as outf:
             outf.write(str(generated_html))
+    Pool.map(run,seeds)
